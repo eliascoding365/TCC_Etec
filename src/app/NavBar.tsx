1112675a19acb7b6b06b27a3369e6ec1bcc0c61a
eliascoding365 <eliasmontan10@gmail.com>
2024-03-13 14:42:42 -0300

    Button modal for adding vaga

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,21 +1,27 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { PiHouseLine } from "react-icons/pi";
+import { PiBooks } from "react-icons/pi";
+import { PiSuitcase } from "react-icons/pi";
+
 import classnames from "classnames";
 
 import React from 'react'
+import AddBtnModal from "./components/AddBtnModal/AddBtnModal";
 
 const NavBar = () => {
   const currentPath = usePathname()
   console.log(currentPath)
   const links = [
-    { label: 'Sobre' , href: '/sobre'},
-    { label: 'Home' , href: '/'}
+    { label: 'Sobre' , href: '/sobre', icon: <PiBooks size={'25px'}/>},
+    { label: 'Home'  , href: '/',      icon: <PiHouseLine size={'25px'} />},
+    //{ label: 'Add'   , href: '/',      icon: <PiPlus size={'25px'}/>}
   ]
   return (
-    <nav className='flex space-x-8 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/"> Logo </Link>
-      <ul className='flex space-x-6'>
+    <nav className='flex justify-between bg-gray-100 space-x-8 border-b mb-5 px-5 h-14 items-center'>
+      <Link href="/"> <PiSuitcase className='text-gray-500'size={'35px'}/> </Link>
+      <ul className='flex  space-x-6'>
         {links.map(link => 
           <Link
           key={link.href}
@@ -26,9 +32,10 @@ const NavBar = () => {
             'hover:text-blue-800 transition-colors':  true
           })}
           >
-          {link.label}
+          {link.icon}
           </Link>
         )}
+        <AddBtnModal/>
       </ul>
     </nav>
     )
---
+++ src/app/components/AddBtnModal/AddBtnModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PiPlus } from "react-icons/pi";
+
+const AddBtnModal = () => {
+  return (
+    <button>
+      <PiPlus size={'25px'}/>
+    </button>
+  )
+}
+
+export default AddBtnModal
