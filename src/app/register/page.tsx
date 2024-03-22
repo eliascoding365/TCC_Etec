a0cd0ae1c9b964f72c8add6a3e2bc52d269efb76
eliascoding365 <eliasmontan10@gmail.com>
2024-03-22 13:56:58 -0300

    creating login and register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterForm from '../components/FormRegister/FormRegister'
+
+const RegisterPage = () => {
+  return (
+    <div>RegisterPage
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default RegisterPage
