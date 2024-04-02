70258b04ef509bb9d78b371508ea0962218ca884
eliascoding365 <eliasmontan10@gmail.com>
2024-04-02 13:29:58 -0300

    Callback for vaga forms when submitting close modal and refreshes the page

--- src/app/components/ButtonOpenModalCreateVaga/ButtonOpenModalCreateVaga.tsx
+++ src/app/components/ButtonOpenModalCreateVaga/ButtonOpenModalCreateVaga.tsx
@@ -1,83 +1,77 @@
-'use client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { PiPlus } from "react-icons/pi";
 import FormVaga from './FormVaga';
 import { useRouter } from 'next/router';
 
 const ButtonOpenModalCreateVaga = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const modalRef = useRef<HTMLDivElement>(null);
 
-
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: { target: any; }) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        closeModal();
-      }
+    const openModal = () => {
+        setIsOpen(true);
     };
 
-    if (isOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-      document.body.classList.add("overflow-y-hidden")
-    } else {
-      document.body.classList.remove("overflow-y-hidden")
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+    const closeModal = () => {
+        setIsOpen(false);
     };
-  }, [isOpen]);
 
+    useEffect(() => {
+        const handleClickOutside = (event: { target: any; }) => {
+            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                closeModal();
+            }
+        };
 
-  return (
-    <div >
-      <div className="
-      flex flex-col justify-center items-center
-      text-xs font-medium
-      text-white opacity-60 hover:text-white hover:opacity-100 transition-opacity">
-        <button
-          className="btn"
-          onClick={openModal}
-          onChange={closeModal}
-        >
-          <PiPlus size={'25px'} />
-          <span>Add</span>
-        </button>
-      </div>
-      {isOpen && (
-        <div className="
-          flex items-center justify-center
-          w-full h-full
-          bg-gray-900 bg-opacity-50
-          backdrop-blur-xs fixed inset-0 z-50 
-          
-          ">
-          <div ref={modalRef} className='
-            flex flex-col absolute
-            w-96 h-2/3 pt-12 m-20 overflow-y-auto
-            bg-gray-200 rounded-xl shadow-2xl 
-            border border-zinc-200
-        ' >
-            <button type="button" className="btn btn-sm btn-circle btn-ghost flex absolute mx-6 my-4 right-0 top-0" onClick={closeModal}>✕</button>
-            <h2 className="flex justify-center text-2xl font-bold mb-4">Criar Vaga</h2>
-            <FormVaga />
-          </div>
+        if (isOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+            document.body.classList.add("overflow-y-hidden");
+        } else {
+            document.body.classList.remove("overflow-y-hidden");
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isOpen]);
+
+    return (
+        <div>
+            <div className="
+            flex flex-col justify-center items-center
+            text-xs font-medium
+            text-white opacity-60 hover:text-white hover:opacity-100 transition-opacity">
+                <button
+                    className="btn"
+                    onClick={openModal}
+                    onChange={closeModal}
+                >
+                    <PiPlus size={'25px'} />
+                    <span>Add</span>
+                </button>
+            </div>
+            {isOpen && (
+                <div className="
+                  flex items-center justify-center
+                  w-full h-full
+                  bg-gray-900 bg-opacity-50
+                  backdrop-blur-xs fixed inset-0 z-50 
+
+                  ">
+                    <div ref={modalRef} className='
+                    flex flex-col absolute
+                    w-96 h-2/3 pt-12 m-20 overflow-y-auto
+                    bg-gray-200 rounded-xl shadow-2xl 
+                    border border-zinc-200
+                ' >
+                        <button type="button" className="btn btn-sm btn-circle btn-ghost flex absolute mx-6 my-4 right-0 top-0" onClick={closeModal}>✕</button>
+                        <h2 className="flex justify-center text-2xl font-bold mb-4">Criar Vaga</h2>
+                        <FormVaga closeModal={closeModal} />
+                    </div>
+                </div>
+            )}
         </div>
-      )}
+    );
+};
 
-
-    </div>
-  )
-}
-
-export default ButtonOpenModalCreateVaga
-
+export default ButtonOpenModalCreateVaga;
--- src/app/components/ButtonOpenModalCreateVaga/FormVaga.tsx
+++ src/app/components/ButtonOpenModalCreateVaga/FormVaga.tsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import ButtonCreateVaga from './ButtonCreateVaga';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface VagaForm {
-    name: string,
-    description: string
+    name: string;
+    description: string;
 }
-const FormVaga = () => {
+
+const FormVaga = ({ closeModal }: { closeModal: () => void }) => {
     const { register, handleSubmit } = useForm<VagaForm>();
+    const router = useRouter();
+    const onSubmit = async (data: VagaForm) => {
+        await axios.post('/api/vaga', data);
+        closeModal(); 
+        router.refresh();
+
+    };
 
     return (
-        <form
-        className='flex flex-col px-6' method="dialog"
-        onSubmit={handleSubmit( async(data) => 
-        await axios.post('/api/vaga', data)
-        )}>
+        <form className='flex flex-col px-6' method="dialog" onSubmit={handleSubmit(onSubmit)}>
             <p className="flex items-center  mb-1 text-gray-700 font-semibold">Nome</p>
             <div className='flex items-center '>
                 <input className='rounded-md w-full px-4 py-2' type="text" {...register('name')} />
             </div>
             <p className="flex items-center  mb-1 mt-1  text-gray-700 font-semibold">Escreva seus intereses</p>
             <div className='flex items-center '>
-                <textarea className='rounded-md w-full h-24 px-4 py-2 resize-none' {...register('description')}  />
+                <textarea className='rounded-md w-full h-24 px-4 py-2 resize-none' {...register('description')} />
             </div>
             <footer className='flex justify-end absolute right-0 bottom-0 mx-6 my-6'>
-              <ButtonCreateVaga/>
+                <ButtonCreateVaga />
             </footer>
         </form>
-    )
-}
+    );
+};
 
-export default FormVaga
+export default FormVaga;
