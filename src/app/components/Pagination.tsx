0e57baf77c8f4407ce090a7bbef6fd2b93b98bb9
eliascoding365 <eliasmontan10@gmail.com>
2024-05-24 10:21:29 -0300

    Implemeting pagination

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,6 +1,9 @@
+'use client'
 import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons'
 import React from 'react'
 import { Button } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 interface Props {
   itemCount: number,
@@ -8,22 +11,58 @@ interface Props {
   currentPage: number
 }
 
-const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+const Pagination = ({
+  itemCount,
+  pageSize,
+  currentPage 
+  }: Props) => {
+    const router = useRouter();
+    const searchParams = useSearchParams()
   const pageCount = Math.ceil(itemCount / pageSize)
   if (pageCount <= 1) return null
+
+  const changePage = (page: number) =>{
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString())
+    router.push('?' + params.toString())
+
+  }
+
   return (
-    <div className='flex items-center gap-2'>
+    <div className='flex items-center gap-2 my-12'>
       <p>Página: {currentPage} de {pageCount}</p>
-      <Button color='gray' variant='soft' disabled={currentPage === 1}>
-        <ChevronLeftIcon />
-      </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === 1}>
+      <Button 
+      color='gray' 
+      variant='soft' 
+      disabled={currentPage === 1}
+      onClick={()=> changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === pageCount}>
+      <Button 
+      color='gray' 
+      variant='soft' 
+      disabled={currentPage === 1}
+      onClick={()=> changePage(currentPage - 1)}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Button 
+      color='gray' 
+      variant='soft' 
+      disabled={currentPage === pageCount}
+      onClick={()=> changePage(currentPage + 1)}
+      
+      >
         <ChevronRightIcon />
       </Button>
-      <Button color='gray' variant='soft' disabled={currentPage === pageCount}>
+      <Button 
+      color='gray' 
+      variant='soft' 
+      disabled={currentPage === pageCount}
+      onClick={()=> changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import CardVaga from "./components/CardVaga/CardVaga";
 import Pagination from "./components/Pagination";
-export default function Home() {
+export default function Home({ searchParams}: {searchParams: { page: string}}) {
   return (
-    <main className='flex items-center justify-center bg-slate-300'>
-        <Pagination itemCount={100} pageSize={10 }currentPage={2}/>
+    <main className='flex flex-col items-center justify-center bg-slate-300'>
+        <Pagination itemCount={100} pageSize={10 }currentPage={parseInt(searchParams.page)}/>
         <CardVaga/>
     </main>
   );
