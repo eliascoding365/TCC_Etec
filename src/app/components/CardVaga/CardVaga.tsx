385523a3625674de6dfbe7d15b77da182d66a4c0
eliascoding365 <eliasmontan10@gmail.com>
2024-06-05 17:43:35 -0300

    Fix error user invocation - fixed when the users not logged in to not render the component, when render post created by id of session

--- src/app/components/CardVaga/CardVaga.tsx
+++ src/app/components/CardVaga/CardVaga.tsx
@@ -16,13 +16,14 @@ interface Props {
 export default async function CardVaga({ searchParams }: Props) {
   const session = await getServerSession();
   const page = parseInt(searchParams.page) || 1;
-  const pageSize = 4
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session?.user.email
-    }
+  const pageSize = 4;
 
-  })
+  const user = session ? await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  }) : null;
+
   const vagas = await prisma.vaga.findMany({
     include: {
       createdBy: {
@@ -35,38 +36,37 @@ export default async function CardVaga({ searchParams }: Props) {
     take: pageSize
   });
 
-  const totalItems = await prisma.vaga.count()
+  const totalItems = await prisma.vaga.count();
+
   return (
     <div className='flex flex-col items-center justify-center mb-4'>
-      <div className='flex flex-col w-auto h-auto max-w-sm m-8 justify-center' >
+      <div className='flex flex-col w-auto h-auto max-w-sm m-8 justify-center'>
         {vagas.map((vaga) => (
           <div key={vaga.id} className='bg-white p-2 rounded mb-14'>
             <div className='flex content-around mx-5 mt-4 mb-8'>
-              <div className='mr-3 '><FaUserCircle size={'43px'} /></div>
+              <div className='mr-3'><FaUserCircle size={'43px'} /></div>
               <div>
-                <h1 >{vaga.createdBy.name}</h1>
+                <h1>{vaga.createdBy.name}</h1>
                 <p className='text-xs font-light'>id:{vaga.createdById}</p>
               </div>
               <div className='ml-auto flex items-center'>
-                { user?.id  === vaga.createdById &&
-                  <>
-                    <button>
-                      <FaPen />
-                    </button>
-                  </>
-                }
+                {session && user?.id === vaga.createdById && (
+                  <button>
+                    <FaPen />
+                  </button>
+                )}
               </div>
             </div>
             <div className='mx-5 mb-6'>
               <p className='text-md'>{vaga.name}</p>
               <Image
                 src="/static/images/cards/temos-vagas.png"
-                width={328}  //271
+                width={328}
                 height={328}
                 alt="Picture of the author"
               />
               <div className='flex justify-end mb-4'>
-                <p className='text-xs font-light '>
+                <p className='text-xs font-light'>
                   <span className='mr-1'>Data da publicação</span>
                   {new Date(vaga.createdAt).toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' })}
                 </p>
